d615dabe12a5d642b5f71d80b3327f459ec1884a
alinealflenpopcode <alflenaline@gmail.com>
2022-07-31 12:33:59 -0300

    feat: product description card implementation

---
+++ devnology/lib/view/widgets/text_card_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TextCardWidget extends StatelessWidget {
+  final String description;
+  final double size;
+
+  const TextCardWidget({
+    required this.description,
+    required this.size,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      description,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
+    );
+  }
+}
