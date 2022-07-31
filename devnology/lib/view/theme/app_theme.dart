37b0a1632bb121833ba8a5f47e5e1256df02176a
alinealflenpopcode <alflenaline@gmail.com>
2022-07-31 12:34:25 -0300

    feat: add new color to theme

--- devnology/lib/view/theme/app_theme.dart
+++ devnology/lib/view/theme/app_theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static const Color primary = Color(0xFF2E3746);
   static const Color secondary = Color(0xFFFFC600);
+  static const Color tertiary = Color(0xFF5A6476);
 
   static ThemeData get theme {
     return ThemeData(
